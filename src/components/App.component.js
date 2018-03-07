f671c6e9af6bdddc76b71775382e2b6937ddea37
bozayi1 <oz.burak1995@gmail.com>
2018-03-07 16:24:52 -0500

    Added Vet Details to the Client Page

--- src/components/App.component.js
+++ src/components/App.component.js
@@ -107,7 +107,7 @@ export default class Main extends React.Component {
 		let pool = await sql.connect(sqlConfig)
 		let t = Date.now()
 		let result = await pool.request()
-			 .query("SELECT * from dbo.Animals, dbo.ClientDetails where dbo.Animals.ClientID = dbo.ClientDetails.ClientID")
+			 .query("SELECT * from dbo.Animals, dbo.VetDetails, dbo.ClientDetails where dbo.Animals.ClientID = dbo.ClientDetails.ClientID and dbo.ClientDetails.VetSurgeryId = dbo.VetDetails.ID")
 		//if err sql.close
 
 
@@ -115,7 +115,7 @@ export default class Main extends React.Component {
 
 		let y = Date.now()
 		let bookings = await pool.request()
-			 .query("SELECT * from dbo.BookingObjects ,dbo.Animals, dbo.ClientDetails where dbo.Animals.ClientID = dbo.ClientDetails.ClientID and dbo.Animals.AnimalID =  dbo.BookingObjects.AnimalID and dbo.BookingObjects.DateOut > '2017-07-06 12:00:00.000'")
+			 .query("SELECT * from dbo.BookingObjects ,dbo.VetDetails, dbo.Animals, dbo.ClientDetails where dbo.Animals.ClientID = dbo.ClientDetails.ClientID and dbo.Animals.AnimalID =  dbo.BookingObjects.AnimalID and dbo.ClientDetails.VetSurgeryId = dbo.VetDetails.ID and dbo.BookingObjects.DateOut > '2017-07-06 12:00:00.000'")
 		//if err sql.close
 
 
@@ -123,6 +123,8 @@ export default class Main extends React.Component {
 			 .query("SELECT top 1 * from dbo.BookingObjects order by BookingID desc")
 		//if err sql.close
 
+		console.log(result.recordset[10].Address1)
+console.log(bookings.recordset)
 		sql.close()
 
 		this.setState({
--- src/components/client/ClientProfile.js
+++ src/components/client/ClientProfile.js
@@ -19,23 +19,32 @@ export default class ClientProfile extends React.Component {
 					<h3>Client Details</h3>
 					Last Name: {this.props.animal.LastName} <br></br>
 					First Name: {this.props.animal.FirstName} Title: {this.props.animal.Title}<br></br>
+					{console.log(this.props.animal)}
 					Partner Name: {this.props.animal.PartnerName} <br></br>
-					Address1: {this.props.animal.Address1} <br></br>
-					Address2: {this.props.animal.Address2} <br></br>
+					Address1: {!Array.isArray(this.props.animal.Address1) ? this.props.animal.Address1 : this.props.animal.Address1[1]} <br></br>
+					Address2: {!Array.isArray(this.props.animal.Address2) ? this.props.animal.Address2 : this.props.animal.Address2[1]} <br></br>
 					Town/City: {this.props.animal.Address3} <br></br>
-					Region/State: {this.props.animal.Region} <br></br>
-					Postcode/ZIP: {this.props.animal.PostcodeZIP} Mailings Permitted: {this.props.animal.Mailings ? 'Yes' : 'No'}<br></br>
+					Region/State: {!Array.isArray(this.props.animal.Region) ? this.props.animal.Region :this.props.animal.Region[1] }<br></br>
+					Postcode/ZIP: {!Array.isArray(this.props.animal.PostcodeZIP) ? this.props.animal.PostcodeZIP : this.props.animal.PostcodeZIP[1]} Mailings Permitted: {this.props.animal.Mailings ? 'Yes' : 'No'}<br></br>
 					Country: {this.props.animal.Country}<br></br>
 					Tel (home): {this.props.animal.TelHome}<br></br>
 					Tel (work): {this.props.animal.TelWork}<br></br>
 					Mobile/Cell: {this.props.animal.CellMobile}<br></br>
-					Email: {this.props.animal.Email} <br></br>
+					Email: {!Array.isArray(this.props.animal.Email) ? this.props.animal.Email : this.props.animal.Email[1]} <br></br>
 					Web Contact: {this.props.animal.WebContact} <br></br>
 					Indetification: {this.props.animal.ClientIdent} <br></br>
 					Referred By: {this.props.animal.Referred} Discount: {this.props.animal.Discount}<br></br>
 					Notes: {this.props.animal.ClientNotes} <br></br>
 					<h4>Animals</h4>
 					<h4>Veterinary</h4>
+					Surgery: {this.props.animal.PracticeName} <br></br>
+					Vet Name: {this.props.animal.VetName} <br></br>
+					Address1: {!Array.isArray(this.props.animal.Address1) ? '' : this.props.animal.Address1[0]} <br></br>
+					Address2: {!Array.isArray(this.props.animal.Address2) ? '' : this.props.animal.Address2[0]} <br></br>
+					Town/City: {this.props.animal.Town} <br></br>
+					Region/State: {!Array.isArray(this.props.animal.Region) ? '' :this.props.animal.Region[0] }<br></br>
+					Postcode/ZIP: {!Array.isArray(this.props.animal.PostcodeZIP) ? '' : this.props.animal.PostcodeZIP[0]}<br></br>
+					Email: {!Array.isArray(this.props.animal.Email) ? '' : this.props.animal.Email[0]} <br></br>
 				</div>
 			);
 		else
