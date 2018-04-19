0624999ba153be12ede0f1af49697dc4baba2f1f
Ali San Kaya <akaya2@binghamton.edu>
2018-04-19 05:37:35 -0400

    layout

--- src/components/pages/calendar/Layout.js
+++ src/components/pages/calendar/Layout.js
@@ -18,6 +18,9 @@ function colorScheme(status){ //color table
 			break;		
 		case "NCO":
 			return "red";
+			break;
+		default:
+			return "grey"
 			break;		
 	}
 }
@@ -27,11 +30,11 @@ export default class Layout extends React.Component {
 		let {bookings_list, current} = this.props;
 		return(
 		<div>
-			<div className = "center"><h2>    Monday     Tuesday   Wednesday   Thursday     Friday      Saturday      Sunday</h2></div>
+			<div className = "center"><h2> Monday     Tuesday  Wednesday Thursday  Friday   Saturday   Sunday</h2></div>
 			<GridLayout className="layout" cols={7} rowHeight={12} width={1000} isResizable = {false} verticalCompact = {false}>
 				{
 				current.map(obj => 
-					<div className = {colorScheme(obj.Status)} key={obj.BookingID} data-grid={{x: obj.DateIn.getDay() - 1, y: obj.KennelID*1, w: obj.Days*1, h: 2}}><b>{obj.AnimalName}/{obj.FirstName} {obj.LastName}</b></div>
+					<div className = {colorScheme(obj.Status)} key={obj.BookingID} data-grid={{x: obj.DateIn.getDay() - 1, y: obj.KennelID*1, w: (true && obj.Days*1) || 1, h: 2}}><b>{obj.AnimalName}/{obj.FirstName} {obj.LastName}</b></div>
 				)
 				}
 			</GridLayout>
