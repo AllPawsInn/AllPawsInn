357a9e556ee0eae95866b1faa07cdf46e00ce051
boz <boz1@binghamton.edu>
2018-04-29 23:21:41 -0400

    updated navbar

--- src/js/sqlconfig.js
+++ src/js/sqlconfig.js
@@ -1,9 +1,8 @@
 let sqlConfig = {
-	user: "sa",
-  password: "password",
-  server: "BURAKFIDANSOY",
-  port: "1433",
-  database: "KMDB"
+	user: 'sa', // your mssql account
+	password: 'asdqwe123',
+	server: 'DESKTOP-9BJOBVM\\SQLEXPRESS', // your server name
+	database: 'KMDB'
 }
 
 module.exports = sqlConfig
