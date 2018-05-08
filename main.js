959b80f8fb59d9a764cdc7a2646104a6d72488a7
Ali San Kaya <akaya2@binghamton.edu>
2018-05-08 11:39:08 -0400

    remove custom titlebar

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ let win;
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 1200, height: 700, frame: false, backgroundColor:"#D3D3D3"});
+  win = new BrowserWindow({width: 1200, height: 700, backgroundColor:"#D3D3D3"});
+  win.setMenu(null)
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
